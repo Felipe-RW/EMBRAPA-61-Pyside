a2f09143410ff8472b7b1ce2033145b3384283f4
eo-lima <enzolimacubo@gmail.com>
2026-08-21 17:29:55 -0400

    primeiro checkbox

--- Telas/popup_relatorio_administrador_tipos_de_acoes.py
+++ Telas/popup_relatorio_administrador_tipos_de_acoes.py
@@ -4,6 +4,9 @@ from PySide6.QtWidgets import (
     QWidget,
     QLabel,
     QPushButton,
+    QFrame,
+    QVBoxLayout,
+    QCheckBox
 )
 from PySide6.QtCore import Qt
  
@@ -73,7 +76,69 @@ botao_relatorio_anos.setStyleSheet("""
         border-radius: 20px;                 
     }
 """)
- 
+
+opcoes_relatorio = QFrame(janela)
+opcoes_relatorio.setGeometry(54, 215, 1094, 354)
+opcoes_relatorio.setStyleSheet("""
+    QWidget {
+    background-color: #DFDFDF; 
+    border-radius: 0px;                             
+}
+""")
+
+acoes = QLabel("Ações", opcoes_relatorio)
+acoes.setGeometry(61, 28, 67, 23)
+acoes.setStyleSheet("""
+    QLabel {
+    color: black;
+    font-size: 20px;
+    font-family: verdana;
+    font-weight: bold 
+}
+""")
+
+botao_selecionar_acoes = QPushButton("Selecionar Ações", opcoes_relatorio)
+botao_selecionar_acoes.setGeometry(61, 74, 192, 30)
+botao_selecionar_acoes.setStyleSheet("""
+    QPushButton {
+    color: white;
+    font-size: 13;
+    font-family: verdana;
+    font-weight: bold;
+    background-color: #356394;
+    border-radius: 15px                          
+}
+""")
+
+botao_todas_as_acoes = QPushButton("Todas as Ações", opcoes_relatorio)
+botao_todas_as_acoes.setGeometry(275, 74, 192, 30)
+botao_todas_as_acoes.setStyleSheet("""
+    QPushButton {
+    color: black;
+    font-size: 13;
+    font-family: verdana;
+    font-weight: bold;
+    background-color: white;
+    border-radius: 15px                          
+}
+""")
+
+selecionar_acao1 = QCheckBox("Coordenação de evento REGIONAL", opcoes_relatorio)
+selecionar_acao1.setGeometry(61, 120, 400, 50)
+selecionar_acao1.setStyleSheet("""
+    QCheckBox {
+    color: black;
+    font-size: 16px;
+    font-family: verdana;
+    font-weight: bold
+             
+
+
+
+
+}
+""")
+
 janela.show()
  
 sys.exit(app.exec())
