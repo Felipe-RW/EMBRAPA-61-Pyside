3263b1ddb3cd736a556662576b53bb586720adf1
Theo1907631 <thenriqueortiz@gmail.com>
2026-08-21 15:29:01 -0400

    código finalizado

--- Telas/popup_de_aviso_de_ano_criado.py
+++ Telas/popup_de_aviso_de_ano_criado.py
@@ -1,135 +1,58 @@
-import sys
-
 from PySide6.QtCore import Qt
-from PySide6.QtWidgets import (
-    QApplication,
-    QLabel,
-    QPushButton,
-    QVBoxLayout,
-    QWidget
-)
+from PySide6.QtWidgets import QLabel, QPushButton, QVBoxLayout, QWidget
+import estilo
 
-
-class PopupSucesso(QWidget):
-
+class PopupDeAvisoDeAnoCriado(QWidget):
     def __init__(self):
         super().__init__()
 
+        
+        self.setWindowFlags(Qt.WindowType.FramelessWindowHint)
+        self.setAttribute(Qt.WidgetAttribute.WA_TranslucentBackground)
+
         self.setWindowTitle("Sucesso")
         self.setFixedSize(1000, 650)
+        self.setStyleSheet(estilo.ESTILO_Geral)
 
-        self.setStyleSheet("""
-            QWidget {
-                background-color: white;
-            }
-        """)
+        
+        self.container = QWidget(self)
+        self.container.setObjectName("container")
+        self.container.resize(1000, 550)
 
-        layout = QVBoxLayout()
-
+        
+        layout = QVBoxLayout(self.container)
         layout.setContentsMargins(0, 40, 0, 60)
         layout.setSpacing(0)
         layout.setAlignment(Qt.AlignmentFlag.AlignTop)
 
-        # Círculo
+        
         circulo = QLabel("✓")
         circulo.setFixedSize(210, 210)
         circulo.setAlignment(Qt.AlignmentFlag.AlignCenter)
+        circulo.setStyleSheet(estilo.ESTILO_Circulo)
 
-        circulo.setStyleSheet("""
-            QLabel {
-                background-color:#d9ebff;
-                border-radius: 105px;
-                font-size: 90px;
-            }
-        """)
-
-        # Título
         titulo = QLabel("O ano foi criado!")
-
         titulo.setAlignment(Qt.AlignmentFlag.AlignCenter)
+        titulo.setStyleSheet(estilo.ESTILO_Titulo)
 
-        titulo.setStyleSheet("""
-            QLabel {
-                background: green;
-                font-size: 30px;
-                font-weight: 700;
-            }
-        """)
+        msg1 = QLabel("Ano adicionado ao sistema com sucesso!")
+        msg1.setAlignment(Qt.AlignmentFlag.AlignCenter)
+        msg1.setStyleSheet(estilo.ESTILO_Mensagem1)
 
-
-    # Texto 1
-        mensagem = QLabel(
-            "As informações do ano foram atualizadas!"
-        )
-
-        mensagem.setAlignment(Qt.AlignmentFlag.AlignCenter)
-
-        mensagem.setStyleSheet("""
-            QLabel {
-                background: white;
-                font-size: 21px;
-                color: #444;
-            }
-        """)
-
-        # Texto 2
-        mensagem = QLabel(
-            "Alterações realizadas com sucesso!"
-        )
-
-        mensagem.setAlignment(Qt.AlignmentFlag.AlignCenter)
-
-        mensagem.setStyleSheet("""
-            QLabel {
-                background: white;
-                font-size: 18px;
-                color: #444;
-            }
-        """)
-
-        # Botão
+        
         botao = QPushButton("Fechar")
-
         botao.setFixedSize(370, 56)
-
-        botao.setStyleSheet("""
-            QPushButton {
-                background-color: #1f2d87;
-                color: white;
-                border: none;
-                border-radius: 28px;
-                font-size: 18px;
-            }
-        """)
-
+        botao.setStyleSheet(estilo.ESTILO_Botao)
         botao.clicked.connect(self.close)
 
-        layout.addWidget(
-            circulo,
-            alignment=Qt.AlignmentFlag.AlignHCenter
-        )
-
+        
+        layout.addWidget(circulo, alignment=Qt.AlignmentFlag.AlignHCenter)
         layout.addSpacing(35)
-
         layout.addWidget(titulo)
-
         layout.addSpacing(10)
-
-        layout.addWidget(mensagem)
-
-        layout.addSpacing(180)
-
-        layout.addWidget(
-            botao,
-            alignment=Qt.AlignmentFlag.AlignHCenter
-        )
+        layout.addWidget(msg1)
+        layout.addSpacing(5)
+        layout.addSpacing(50)
+        layout.addWidget(botao, alignment=Qt.AlignmentFlag.AlignHCenter)
 
         self.setLayout(layout)
-
-
-app = QApplication(sys.argv)
-
-janela = PopupSucesso()
-janela.show()
-
-sys.exit(app.exec())
