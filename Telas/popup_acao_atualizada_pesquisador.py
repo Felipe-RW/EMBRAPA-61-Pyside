7fa039ee2ad36dab55b92b889be4b0a5f88035c2
ArthurFerrazBitencourtDeRezende <arthurferrazbitencourt0501@gmail.com>
2026-08-24 15:18:28 -0400

    Popup de ação atualizada foi corrigido e já está pronto agora.

--- Telas/popup_acao_atualizada_pesquisador.py
+++ Telas/popup_acao_atualizada_pesquisador.py
@@ -7,79 +7,94 @@ from PySide6.QtWidgets import (
 )
 from PySide6.QtCore import Qt
 
+
+class PopupAcaoAtualizada(QWidget):
+
+    def __init__(self):
+        super().__init__()
+
+        self.setWindowTitle("Ação atualizada")
+        self.setFixedSize(1200, 640)
+
+        self.setStyleSheet("""
+            QWidget {
+                background-color: white;
+                border-radius: 20px;
+            }
+        """)
+
+        self.circulo = QLabel(self)
+        self.circulo.setGeometry(475, 55, 238, 238)
+        self.circulo.setAlignment(Qt.AlignCenter)
+        self.circulo.setText("✓")
+
+        self.circulo.setStyleSheet("""
+            QLabel {
+                background-color: #C4E1FF;
+                color: #356394;
+                border: none;
+                border-radius: 119px;
+                font-size: 95px;
+                font-weight: normal;
+            }
+        """)
+
+        self.titulo = QLabel("Sua ação foi atualizada!", self)
+        self.titulo.setGeometry(350, 330, 500, 40)
+        self.titulo.setAlignment(Qt.AlignCenter)
+
+        self.titulo.setStyleSheet("""
+            QLabel {
+                color: black;
+                font-size: 34px;
+                font-family: Verdana;
+                font-weight: bold;
+                background-color: transparent;
+            }
+        """)
+
+        self.texto = QLabel(
+            "Sua ação foi reenviada para os avaliadores.",
+            self
+        )
+        self.texto.setGeometry(300, 375, 600, 30)
+        self.texto.setAlignment(Qt.AlignCenter)
+
+        self.texto.setStyleSheet("""
+            QLabel {
+                color: black;
+                font-size: 24px;
+                font-family: Verdana;
+                font-weight: normal;
+                background-color: transparent;
+            }
+        """)
+
+        self.botao = QPushButton("Fechar", self)
+        self.botao.setGeometry(460, 550, 285, 48)
+
+        self.botao.setStyleSheet("""
+            QPushButton {
+                background-color: #172b8c;
+                color: white;
+                border: none;
+                border-radius: 20px;
+                font-size: 23px;
+            }
+
+            QPushButton:hover {
+                background-color: #056510;
+            }
+
+            QPushButton:pressed {
+                background-color: #056510;
+            }
+        """)
+
+
 app = QApplication(sys.argv)
 
-janela = QWidget()
-janela.setWindowTitle("Ação atualizada")
-janela.setFixedSize(1200, 640 )
-janela.setStyleSheet("""
-    QWidget {
-        background-color: white;
-        border-radius: 20px
-     
-    }
-""")
-
-circulo = QLabel(janela)
-circulo.setGeometry(475, 55, 238, 238)
-circulo.setAlignment(Qt.AlignCenter)
-circulo.setText("✓")
-circulo.setStyleSheet("""
-    QLabel {
-        background-color: #C4E1FF;
-        color: #356394;
-        border: none;
-        border-radius: 119px;
-        font-size: 95px;
-        font-weight: normal;
-    }
-""")
-
-titulo = QLabel("Sua ação foi atualizada!", janela)
-titulo.setGeometry(350, 330, 500, 40)
-titulo.setAlignment(Qt.AlignCenter)
-titulo.setStyleSheet(""" 
-    QWidget {
-        color: black;
-        font-size: 34px;
-        font-family: verdana;
-        font-weight: bold;
-        background-color: transparent;             
-    }
-""")
-
-texto = QLabel("Sua ação foi reenviada para os avaliadores.", janela)
-texto.setGeometry(300, 375, 600, 30)
-texto.setAlignment(Qt.AlignCenter)
-texto.setStyleSheet("""
-    QWidget {
-        color: black;
-        font-size: 24px;
-        font-family: verdana;
-        font-weight: regular;
-        background-color: transparent;
-    }
-""")
-
-botao = QPushButton("Fechar", janela)
-botao.setGeometry(460, 550, 285, 48)
-botao.setStyleSheet("""
-    QPushButton {
-        background-color: #172b8c;
-        color: white;
-        border: none;
-        border-radius: 20px;
-        font-size: 23px;
-    }
-    QPushButton:hover {
-        background-color: #056510;
-    }
-    QPushButton:pressed {
-        background-color: #056510;
-    }
-""")
-
-
+janela = PopupAcaoAtualizada()
 janela.show()
 
 sys.exit(app.exec())
