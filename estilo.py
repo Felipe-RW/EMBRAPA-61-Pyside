e908e70c2eb9bf058868c791d55f4c8bac945520
Victor Alexandre da Silva <victor.alexandre9813@gmail.com>
2026-08-28 17:06:37 -0400

    Aula do Mauricio Dia 28/08/2026

---
+++ estilo.py
@@ -0,0 +1,54 @@
+ESTILO_Geral = """
+    #container {
+        background-color: white;
+        border-radius: 30px;
+        border: 1px solid #d1d5db;
+    }
+"""
+
+ESTILO_Circulo = """
+    QLabel {
+        background-color: #d9ebff;
+        border-radius: 105px;
+        font-size: 90px;
+        color: #1f2d87;
+    }
+"""
+
+ESTILO_Titulo = """
+    QLabel {
+        background: white;
+        font-size: 30px;
+        font-weight: 700;
+        color: #222;
+    }
+"""
+
+ESTILO_Mensagem1 = """
+    QLabel {
+        background: white;
+        font-size: 21px;
+        color: #444;
+    }
+"""
+
+ESTILO_Mensagem2 = """
+    QLabel {
+        background: white;
+        font-size: 18px;
+        color: #777;
+    }
+"""
+
+ESTILO_Botao = """
+    QPushButton {
+        background-color: #1f2d87;
+        color: white;
+        border: none;
+        border-radius: 28px;
+        font-size: 18px;
+    }
+    QPushButton:hover {
+        background-color: #2a3cb0;
+    }
+"""
