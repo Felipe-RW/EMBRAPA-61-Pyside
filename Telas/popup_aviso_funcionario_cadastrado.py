667514210d25cdf71b6d2f894aca0f4819cce988
eo-lima <enzolimacubo@gmail.com>
2026-08-28 16:31:41 -0400

    finalizando o popup de aviso funcionario cadastrado

--- Telas/popup_aviso_funcionario_cadastrado.py
+++ Telas/popup_aviso_funcionario_cadastrado.py
@@ -4,96 +4,87 @@ from PySide6.QtWidgets import (
     QWidget,
     QLabel,
     QPushButton,
+    QFrame,
+    QVBoxLayout,
+    QCheckBox,
+    QLineEdit
 )
 from PySide6.QtCore import Qt
  
- 
-class PopupAcaoAtualizada(QWidget):
- 
-    def __init__(self):
-        super().__init__()
- 
-        self.setWindowTitle("Funcionário Cadastrado")
-        self.setFixedSize(1202, 639)
- 
-        self.setStyleSheet("""
-            QWidget {
-                background-color: white;
-                border-radius: 20px;
-            }
-        """)
- 
-        self.circulo = QLabel(self)
-        self.circulo.setGeometry(475, 55, 238, 238)
-        self.circulo.setAlignment(Qt.AlignCenter)
-        self.circulo.setText("✓")
- 
-        self.circulo.setStyleSheet("""
-            QLabel {
-                background-color: #C4E1FF;
-                color: #356394;
-                border: none;
-                border-radius: 119px;
-                font-size: 95px;
-                font-weight: normal;
-            }
-        """)
- 
-        self.titulo = QLabel("Funcionário cadastrado com sucesso!", self)
-        self.titulo.setGeometry(250, 330, 800, 40)
-        self.titulo.setAlignment(Qt.AlignCenter)
-        self.titulo.setStyleSheet("""
-            QLabel {
-                color: black;
-                font-size: 34px;
-                font-family: Verdana;
-                font-weight: bold;
-                background-color: transparent;
-            }
-        """)
- 
-        self.texto = QLabel(
-            "O empregado foi adicionado ao sistema.",
-            self
-        )
-        self.texto.setGeometry(300, 375, 600, 30)
-        self.texto.setAlignment(Qt.AlignCenter)
- 
-        self.texto.setStyleSheet("""
-            QLabel {
-                color: black;
-                font-size: 24px;
-                font-family: Verdana;
-                font-weight: normal;
-                background-color: transparent;
-            }
-        """)
- 
-        self.botao = QPushButton("Fechar", self)
-        self.botao.setGeometry(460, 550, 285, 48)
- 
-        self.botao.setStyleSheet("""
-            QPushButton {
-                background-color: #172b8c;
-                color: white;
-                border: none;
-                border-radius: 20px;
-                font-size: 23px;
-            }
- 
-            QPushButton:hover {
-                background-color: #056510;
-            }
- 
-            QPushButton:pressed {
-                background-color: #056510;
-            }
-        """)
- 
- 
 app = QApplication(sys.argv)
  
-janela = PopupAcaoAtualizada()
+janela = QWidget()
+janela.setWindowTitle("Popup Relatório Administrador Tipos de Ações")
+janela.setFixedSize(1202, 639)
+janela.setStyleSheet("""
+    QWidget {
+        background-color: white;
+        border-radius: 20px
+     
+    }
+""")
+
+circulo = QLabel("✓", janela)
+circulo.setGeometry(475, 55, 238, 238)
+circulo.setAlignment(Qt.AlignCenter)
+circulo.setStyleSheet ("""
+    QLabel {
+        background-color: #C4E1FF;          
+        border-radius: 119px;    
+        font-size: 95px;
+        text-align: center;
+        justify-content: center;         
+        color: #356694;
+}
+""")
+
+titulo = QLabel("Empregado cadastrado com sucesso!", janela)
+titulo.setGeometry(245, 330, 720, 50)
+titulo.setAlignment(Qt.AlignCenter)
+titulo.setStyleSheet("""
+    QLabel{
+        font-size: 34px;
+        font-family: 'Verdana', sans-serif;
+        font-weight: bold;    
+        width: 100%;                 
+        text-align: center;
+}
+""")
+
+subtitulo = QLabel("O empregado foi adicionado ao sistema.", janela)
+subtitulo.setGeometry(245, 380, 720, 30)
+subtitulo.setAlignment(Qt.AlignCenter)
+subtitulo.setStyleSheet("""
+    QLabel {
+        font-size: 24px;
+        font-family: 'Verdana', sans-serif;
+        font-weight: lighter;
+}
+""")
+
+botao_fechar = QPushButton("Fechar", janela)
+botao_fechar.setGeometry(460, 550, 285, 48)
+botao_fechar.setStyleSheet("""
+    QPushButton{
+        background-color: #172b8c;
+        color: white;
+        border: none;
+        border-radius: 20px;
+        font-size: 23px;
+    }
+
+    QPushButton:hover {
+        background-color: #056510;
+    }
+
+    QPushButton:pressed {
+        background-color: #056510;
+    }
+""")
+                        
+
+
+
 janela.show()
  
 sys.exit(app.exec())
