ecb0949f9390daacb3e2531a754a6c22035c69d4
Victor Alexandre da Silva <victor.alexandre9813@gmail.com>
2026-09-09 17:03:03 -0400

    Aula do Mauricio Dia 09/09/2026

--- Telas/modelo_tela_administrador.py
+++ Telas/modelo_tela_administrador.py
@@ -148,13 +148,27 @@ class ModeloTelaAdministrador(QMainWindow):
 
         self.botaostacked = QStackedWidget()
 
+        self.home = pagina_principal ()
+        self.calendario = tela_de_anos ()
         self.acoes = Janelinha ()
         self.empregados = tela_de_empregados ()
+        self.validadores = tela_dos_validadores ()
 
+        self.botaostacked.addWidget (self.home)
+        self.botaostacked.addWidget (self.calendario)
         self.botaostacked.addWidget (self.acoes)
         self.botaostacked.addWidget (self.empregados)
+        self.botaostacked.addWidget (self.validadores)
 
         layout.addWidget (self.botaostacked)
+
+        self.btn_home.clicked.connect (
+            lambda: self.botaostacked.setCurrentWidget (self.home)
+        )
+        
+        self.btn_calendario.clicked.connect (
+            lambda: self.botaostacked.setCurrentWidget (self.calendario)
+        )
 
         self.btn_acoes.clicked.connect (
             lambda: self.botaostacked.setCurrentWidget (self.acoes)
@@ -163,6 +177,12 @@ class ModeloTelaAdministrador(QMainWindow):
         self.btn_empregados.clicked.connect (
             lambda: self.botaostacked.setCurrentWidget (self.empregados)
         )
+
+        self.btn_validadores.clicked.connect (
+            lambda: self.botaostacked.setCurrentWidget (self.validadores)
+        )
+
+        paginaprincipal.setCurrentIndex(self.home)
 
         # pagina_home = QFrame()
         # pagina_calendario = QFrame()
