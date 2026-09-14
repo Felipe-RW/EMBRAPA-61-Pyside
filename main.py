dd69a64de9dd12e916cbaaca5bc672d56a3cf21a
eo-lima <enzolimacubo@gmail.com>
2026-09-14 13:57:40 -0400

    apenas colocando os arquivos dentro da apsta

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import sys
 
 from PySide6.QtWidgets import QApplication
 
-from popup_relatorio import PopupRelatorio
+from Telas.popup_relatorio import PopupRelatorio
 
 
 from PySide6.QtWidgets import QLineEdit
