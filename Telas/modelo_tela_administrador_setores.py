496fcbc290e381d63432c71957f3cd476ad7f351
Pedro Menani <pedromenani03@gmail.com>
2026-09-21 17:33:01 -0400

    arrumar hover e border radius

--- Telas/modelo_tela_administrador_setores.py
+++ Telas/modelo_tela_administrador_setores.py
@@ -1,6 +1,6 @@
 import sys, os
 from PySide6.QtCore import Qt
-from PySide6.QtGui import QStandardItemModel, QStandardItem, QPixmap, QColor, QBrush
+from PySide6.QtGui import QStandardItemModel, QStandardItem, QPixmap, QColor, QRegion, QPainterPath
 from PySide6.QtWidgets import (
     QApplication, QWidget, QLabel, QLineEdit, QTextEdit, 
     QComboBox, QPushButton, QVBoxLayout, QHBoxLayout, 
@@ -29,16 +29,16 @@ class ModeloTelaAdministrador(QMainWindow):
             }
         """)
 
-        menu_lateral = QWidget(self)
-        menu_lateral.setGeometry(0, 0, 280, 1080)
-        menu_lateral.setStyleSheet("""
+        menuLateral = QWidget(self)
+        menuLateral.setGeometry(0, 0, 280, 1080)
+        menuLateral.setStyleSheet("""
             QWidget{
                 background-color: #356394
             }
         """)
 
-        menu_lateral_layout = QVBoxLayout(menu_lateral)
-        menu_lateral_layout.setContentsMargins(30, 0, 0, 0)
+        menuLateralLayout = QVBoxLayout(menuLateral)
+        menuLateralLayout.setContentsMargins(30, 0, 0, 0)
 
         self.btn_home = btn_layout (os.path.join(BASE, "Imagens/Painel-Principal-Icone.png"), "Painel Principal")
         self.btn_calendario = btn_layout (os.path.join(BASE, "Imagens/Calendario-Icone.png"), "Calendário")
@@ -46,32 +46,32 @@ class ModeloTelaAdministrador(QMainWindow):
         self.btn_empregados = btn_layout (os.path.join(BASE, "Imagens/Empregados-Icone.png"), "Empregados")
         self.btn_validadores = btn_layout (os.path.join(BASE, "Imagens/Validadores-Icone.png"), "Validadores")
 
-        logo_label = QLabel ()
+        logoLabel = QLabel ()
         logo = QPixmap (LOGO)
-        logo_certa = logo.scaled (220, 190, Qt.KeepAspectRatio, Qt.SmoothTransformation)
-        logo_label.setPixmap (logo_certa)
-        logo_label.setAlignment (Qt.AlignLeft)
+        logoCerta = logo.scaled (220, 190, Qt.KeepAspectRatio, Qt.SmoothTransformation)
+        logoLabel.setPixmap (logoCerta)
+        logoLabel.setAlignment (Qt.AlignLeft)
         
-        menu_lateral_layout.addWidget(logo_label)
-        menu_lateral_layout.addWidget(self.btn_home)
-        menu_lateral_layout.setSpacing(5)
-        menu_lateral_layout.addWidget(self.btn_calendario)
-        menu_lateral_layout.setSpacing(5)
-        menu_lateral_layout.addWidget(self.btn_acoes)
-        menu_lateral_layout.setSpacing(5)
-        menu_lateral_layout.addWidget(self.btn_empregados)
-        menu_lateral_layout.setSpacing(5)
-        menu_lateral_layout.addWidget(self.btn_validadores)
+        menuLateralLayout.addWidget(logoLabel)
+        menuLateralLayout.addWidget(self.btn_home)
+        menuLateralLayout.setSpacing(5)
+        menuLateralLayout.addWidget(self.btn_calendario)
+        menuLateralLayout.setSpacing(5)
+        menuLateralLayout.addWidget(self.btn_acoes)
+        menuLateralLayout.setSpacing(5)
+        menuLateralLayout.addWidget(self.btn_empregados)
+        menuLateralLayout.setSpacing(5)
+        menuLateralLayout.addWidget(self.btn_validadores)
             
-        self.grupo_botoes = QButtonGroup(self)
-        self.grupo_botoes.setExclusive(True)
-        self.grupo_botoes.addButton(self.btn_home)
-        self.grupo_botoes.addButton(self.btn_calendario)
-        self.grupo_botoes.addButton(self.btn_acoes)
-        self.grupo_botoes.addButton(self.btn_empregados)
-        self.grupo_botoes.addButton(self.btn_validadores)
+        self.grupoBotoes = QButtonGroup(self)
+        self.grupoBotoes.setExclusive(True)
+        self.grupoBotoes.addButton(self.btn_home)
+        self.grupoBotoes.addButton(self.btn_calendario)
+        self.grupoBotoes.addButton(self.btn_acoes)
+        self.grupoBotoes.addButton(self.btn_empregados)
+        self.grupoBotoes.addButton(self.btn_validadores)
         
-        menu_lateral_layout.addStretch()
+        menuLateralLayout.addStretch()
 
         cabecalho = QWidget(self)
         cabecalho.setGeometry(280, 0, 1640, 70)
@@ -81,9 +81,9 @@ class ModeloTelaAdministrador(QMainWindow):
             }
         """)
 
-        nome_empregado = QLabel("Fulano da Silva Rodrigues", cabecalho)
-        nome_empregado.setGeometry(35, 22, 400, 30)
-        nome_empregado.setStyleSheet("""
+        nomeEmpregado = QLabel("Fulano da Silva Rodrigues", cabecalho)
+        nomeEmpregado.setGeometry(35, 22, 400, 30)
+        nomeEmpregado.setStyleSheet("""
             QLabel{
                 font-size: 24px;
                 color: #ffffff;
@@ -99,18 +99,18 @@ class ModeloTelaAdministrador(QMainWindow):
             }
         """)
 
-        funcao_empregado = QLabel("Administrador", cabecalho)
-        funcao_empregado.setGeometry(470, 22, 200, 30)
-        funcao_empregado.setStyleSheet("""
+        funcaoEmpregado = QLabel("Administrador", cabecalho)
+        funcaoEmpregado.setGeometry(470, 22, 200, 30)
+        funcaoEmpregado.setStyleSheet("""
             QLabel{
                 color: #ffffff;
                 font-size: 24px
             }
         """)
 
-        nome_tela = QLabel("Validadores", cabecalho)
-        nome_tela.setGeometry(1000, 22, 300, 30)
-        nome_tela.setStyleSheet("""
+        nomeTela = QLabel("Validadores", cabecalho)
+        nomeTela.setGeometry(1000, 22, 300, 30)
+        nomeTela.setStyleSheet("""
             QLabel{
                 color: #ffffff;
                 font-size: 20px;
@@ -118,9 +118,9 @@ class ModeloTelaAdministrador(QMainWindow):
             }
         """)
 
-        botao_logout = QPushButton("Logout", cabecalho)
-        botao_logout.setGeometry(1450, 15, 150, 40)
-        botao_logout.setStyleSheet("""
+        botaoLogout = QPushButton("Logout", cabecalho)
+        botaoLogout.setGeometry(1450, 15, 150, 40)
+        botaoLogout .setStyleSheet("""
             QPushButton{
                 background-color: #ffffff;
                 color: #08175C;
@@ -130,9 +130,9 @@ class ModeloTelaAdministrador(QMainWindow):
             }
         """)
 
-        self.paginaprincipal = QFrame(self)
-        self.paginaprincipal.setGeometry(280, 70, 1600, 1010)
-        self.paginaprincipal.setStyleSheet("""
+        self.paginaPrincipal = QFrame(self)
+        self.paginaPrincipal.setGeometry(280, 70, 1600, 1010)
+        self.paginaPrincipal.setStyleSheet("""
             QFrame{
                 background-color: #ffffff;
                 border-top-left-radius: 20px;
@@ -140,7 +140,7 @@ class ModeloTelaAdministrador(QMainWindow):
             }
         """)
 
-        titulo = QLabel("Validadores", self.paginaprincipal)
+        titulo = QLabel("Validadores", self.paginaPrincipal)
         titulo.setAlignment(Qt.AlignCenter)
         titulo.setGeometry(675, 40, 250, 50)
         titulo.setStyleSheet("""
@@ -153,25 +153,23 @@ class ModeloTelaAdministrador(QMainWindow):
         self.conteudo_pagina()
 
     def conteudo_pagina(self):
-
-        fundo = QFrame(self.paginaprincipal)
+        fundo = QFrame(self.paginaPrincipal)
         fundo.setFrameShape(QFrame.Shape.NoFrame)
         fundo.setGeometry(50, 120, 1500, 840) 
         fundo.setStyleSheet("QFrame { background-color: transparent; }")
 
-        layout_interno = QVBoxLayout(fundo)
-        layout_interno.setContentsMargins(0, 0, 0, 0)
-        layout_interno.setAlignment(Qt.AlignmentFlag.AlignTop)
+        layoutInterno = QVBoxLayout(fundo)
+        layoutInterno.setContentsMargins(0, 0, 0, 0)
+        layoutInterno.setAlignment(Qt.AlignmentFlag.AlignTop)
 
+        sublayouInternoSuperior = QHBoxLayout()
+        sublayouInternoSuperior.setContentsMargins(0, 0, 0, 0)
+        sublayouInternoSuperior.setAlignment(Qt.AlignmentFlag.AlignVCenter)
 
-        sublayou_interno_superior= QHBoxLayout()
-        sublayou_interno_superior.setContentsMargins(0, 0, 0, 0)
-        sublayou_interno_superior.setAlignment(Qt.AlignmentFlag.AlignVCenter)
-
-        novo_setor= QPushButton("Novo setor")
-        novo_setor.setFixedSize(186,52)
-        novo_setor.setStyleSheet("""
-            QPushButton{
+        novoSetor = QPushButton("Novo setor")
+        novoSetor.setFixedSize(186, 52)
+        novoSetor.setStyleSheet("""
+            QPushButton {
                 background-color: #058914;
                 border-radius: 5px;
                 border: none;
@@ -187,76 +185,78 @@ class ModeloTelaAdministrador(QMainWindow):
                 background-color: #04620F;
             }
         """)
-        sublayou_interno_superior.addWidget(novo_setor)
+        sublayouInternoSuperior.addWidget(novoSetor)
 
-        sublayou_interno_superior.addStretch()
+        sublayouInternoSuperior.addStretch()
         
-        barra_pesquisa= QLineEdit()
-        barra_pesquisa.setPlaceholderText("Pesquise...")
-        barra_pesquisa.setFixedSize(358,50)
-        barra_pesquisa.setStyleSheet("""
-            QLineEdit{
+        barraPesquisa = QLineEdit()
+        barraPesquisa.setPlaceholderText("Pesquise...")
+        barraPesquisa.setFixedSize(358, 50)
+        barraPesquisa.setStyleSheet("""
+            QLineEdit {
                 background-color: #ffffff;
                 border-radius: 5px;
                 border: 1px solid;
                 border-color: #686868;
                 color: #B3B3B3;
-                font-size: 20 px;
+                font-size: 20px;
                 font-weight: italic;
             }
         """)
-        sublayou_interno_superior.addWidget(barra_pesquisa)
+        sublayouInternoSuperior.addWidget(barraPesquisa)
 
+        sublayouInternoInferior = QVBoxLayout()
+        sublayouInternoInferior.setContentsMargins(0, 100, 0, 0)
 
+        listaSetores = ["SPT", "SPIT", "NCO"]
+        quantLinhas = len(listaSetores)
 
+        tabelaSetores = QTableWidget(fundo)
+        tabelaSetores.setFixedSize(1500, 500) 
+        tabelaSetores.setContentsMargins(0, 0, 0, 0)
+        tabelaSetores.setColumnCount(2)     
+        tabelaSetores.setRowCount(quantLinhas)        
+
+        tabelaSetores.setEditTriggers(QTableWidget.EditTrigger.NoEditTriggers)
+        tabelaSetores.setFocusPolicy(Qt.FocusPolicy.NoFocus)                     
+        tabelaSetores.setSelectionMode(QTableWidget.SelectionMode.NoSelection)   
+        tabelaSetores.setShowGrid(True) 
         
-        sublayou_interno_inferior= QVBoxLayout()
-        sublayou_interno_inferior.setContentsMargins(0, 100, 0, 0)
+        tabelaSetores.setAlternatingRowColors(False)
 
-        quant_linhas= 5
+        tabelaSetores.horizontalHeader().setVisible(True)
+        tabelaSetores.verticalHeader().setVisible(False)
+        tabelaSetores.setHorizontalHeaderLabels(["Setor", "Status"])
+        tabelaSetores.setColumnWidth(0, 750) 
+        tabelaSetores.setColumnWidth(1, 750) 
 
-
-
-
-        lista_setores = ["SPT", "SPIT", "NCO"]
-        quant_linhas = len(lista_setores)
-
-        tabela_setores = QTableWidget(fundo)
-        tabela_setores.setFixedSize(1500, 500) 
-        tabela_setores.setContentsMargins(0,0,0,0)
-        tabela_setores.setColumnCount(2)     
-        tabela_setores.setRowCount(quant_linhas)        
-
-        tabela_setores.setEditTriggers(QTableWidget.EditTrigger.NoEditTriggers)
-        tabela_setores.setFocusPolicy(Qt.FocusPolicy.NoFocus)                     
-        tabela_setores.setSelectionMode(QTableWidget.SelectionMode.NoSelection)   
-        tabela_setores.setShowGrid(True) 
+        tabelaSetores.setHorizontalScrollBarPolicy(Qt.ScrollBarPolicy.ScrollBarAlwaysOff)
+        tabelaSetores.setVerticalScrollBarPolicy(Qt.ScrollBarPolicy.ScrollBarAlwaysOff)
         
-        tabela_setores.setAlternatingRowColors(True)
-
-        tabela_setores.horizontalHeader().setVisible(True)
-        tabela_setores.verticalHeader().setVisible(False)
-        tabela_setores.setHorizontalHeaderLabels(["Setor", "Status"])
-        tabela_setores.setColumnWidth(0, 750) 
-
-        tabela_setores.setColumnWidth(1, 750) 
-
-        tabela_setores.setHorizontalScrollBarPolicy(Qt.ScrollBarPolicy.ScrollBarAlwaysOff)
-        tabela_setores.setVerticalScrollBarPolicy(Qt.ScrollBarPolicy.ScrollBarAlwaysOff)
-
-        tabela_setores.setStyleSheet("""
+        # O seletor QTableWidget::item:hover foi removido para tirar o efeito de hover
+        tabelaSetores.setStyleSheet("""
             QTableWidget {
-                background-color: #ffffff; /* Cor padrão para as linhas PARES (ex: SPT, NCO) */
+                background-color: #ffffff;
                 border: none;
-                color: #000000;      
                 font-size: 20px;     
                 font-weight: bold;
-                border-radius: 10px;   
+                border-radius: 15px;
+            }
+            
+            QHeaderView {
+                background-color: transparent;
+                border-top-left-radius: 15px;
+                border-top-right-radius: 15px;
+                border: none;
             }
 
-            QTableWidget::item:selected {
-                background-color: transparent;
+            QHeaderView::section:horizontal:first {
+                border-top-left-radius: 15px;
             }
+            QHeaderView::section:horizontal:last {
+                border-top-right-radius: 15px;
+            }
+
             QHeaderView::section {
                 background-color: #356394;
                 color: #ffffff;
@@ -267,53 +267,50 @@ class ModeloTelaAdministrador(QMainWindow):
             }
         """)
 
-        for indice, nome_do_setor in enumerate(lista_setores):
-            tabela_setores.setRowHeight(indice, 89)
+        for indice, nome_do_setor in enumerate(listaSetores):
+            tabelaSetores.setRowHeight(indice, 89)
 
             item_setor = QTableWidgetItem(nome_do_setor)
             item_setor.setTextAlignment(Qt.AlignmentFlag.AlignCenter)
             item_setor.setFlags(Qt.ItemFlag.ItemIsEnabled)
-            tabela_setores.setItem(indice, 0, item_setor) 
-
-            container_switch = QWidget()
             
+            # CORES INVERTIDAS: Ímpares (1, 3...) recebem azul e Pares (0, 2...) recebem branco
             if indice % 2 != 0:
-                container_switch.setStyleSheet("background-color: #E9F2FF; border: none; margin: 0px;")
+                cor_fundo = "#E9F2FF"  # Ímpar (Azul)
             else:
-                container_switch.setStyleSheet("background-color: #ffffff; border: none; margin: 0px;")
+                cor_fundo = "#ffffff"  # Par (Branco)
+
+            # Aplica a cor de fundo no texto do setor (Coluna 0)
+            item_setor.setBackground(QColor(cor_fundo))
+            tabelaSetores.setItem(indice, 0, item_setor) 
+
+            # Aplica a mesma cor de fundo no container do Switch (Coluna 1)
+            container_switch = QFrame()
+            container_switch.setStyleSheet(f"""
+                QFrame {{
+                    background-color: {cor_fundo}; 
+                    border-radius: 0px;
+                }}
+            """)
             
-            layout_switch = QHBoxLayout(container_switch)
-            layout_switch.setContentsMargins(0, 0, 0, 0) 
-            layout_switch.setSpacing(0)
-            layout_switch.setAlignment(Qt.AlignmentFlag.AlignCenter) 
+            layoutSwitch = QHBoxLayout(container_switch)
+            layoutSwitch.setContentsMargins(0, 0, 0, 0) 
+            layoutSwitch.setSpacing(0)
+            layoutSwitch.setAlignment(Qt.AlignmentFlag.AlignCenter) 
 
-            botao_switch = QCheckBox()
-            botao_switch.setStyleSheet("background-color: transparent; border: none;") 
+            botaoSwitch = QCheckBox()
+            botaoSwitch.setStyleSheet("background-color: transparent; border: none;") 
             
-            layout_switch.addWidget(botao_switch)
-            tabela_setores.setCellWidget(indice, 1, container_switch) 
-        
-        sublayou_interno_inferior.addWidget(tabela_setores, alignment=Qt.AlignmentFlag.AlignHCenter)
-        sublayou_interno_inferior.addStretch()
+            layoutSwitch.addWidget(botaoSwitch)
+            tabelaSetores.setCellWidget(indice, 1, container_switch)
+
+        sublayouInternoInferior.addWidget(tabelaSetores, alignment=Qt.AlignmentFlag.AlignHCenter)
+        sublayouInternoInferior.addStretch()
+
+        layoutInterno.addLayout(sublayouInternoSuperior)
+        layoutInterno.addLayout(sublayouInternoInferior)
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-        layout_interno.addLayout(sublayou_interno_superior)
-        layout_interno.addLayout(sublayou_interno_inferior)
 
 
 
